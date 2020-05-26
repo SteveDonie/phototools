2fdaf14d74c0920917db1ab5614d9fd3ff1848ea
Steve Donie <steve@donie.us>
2020-05-26 10:55:45 -0500

    moving from old laptop to new

--- goDatical.sh
+++ goDatical.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cp ~/personal/websites/freakylove/index.html ~/albums/index.html
+cp ~/personal/websites/donie.us/index.html ~/albums/index.html
 perl MakeAlbum.pl freakylove.datical
 perl MakeAlbum.pl Incoming.datical
 perl MakeAlbum.pl projects.datical
