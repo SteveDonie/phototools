8aecc9891a2db99d331fc2baf24c4e1eb44476b8
Steve Donie <steve@donie.us>
2024-04-25 07:43:28 -0500

    added more albums

--- goAWS.sh
+++ goAWS.sh
@@ -2,23 +2,32 @@
 echo Making albums and then syncing to S3
 
 current_date_time=$(date +"%m/%d/%Y %H:%M")
-echo "started at $current_date_time" > timing.txt
+echo -e "\nStarted at $current_date_time" > timing.txt
 
 cp ~/personal/donie.us/album-index.html ~/albums/index.html
 
-echo Time to make Incoming >> timing.txt
+echo -e "\nTime to make Incoming" >> timing.txt
 { time perl MakeAlbum.pl Incoming.aws; } 2>> timing.txt
 
-echo Time to make Eastdale >> timing.txt
+echo -e "\nTime to make Eastdale" >> timing.txt
 { time perl MakeAlbum.pl Eastdale.aws; } 2>> timing.txt
 
-echo Time to make Projects >> timing.txt
+echo -e "\nTime to make East Third" >> timing.txt
+{ time perl MakeAlbum.pl EastThird.aws; } 2>> timing.txt
+
+echo -e "\nTime to make Projects" >> timing.txt
 { time perl MakeAlbum.pl projects.aws; } 2>> timing.txt
 
-echo Time to sync to S3 >> timing.txt
+echo -e "\nTime to make ancestry album" >> timing.txt
+{ time perl MakeAlbum.pl ancestry.aws; } 2>> timing.txt
+
+echo -e "\nTime to make Handyman album" >> timing.txt
+{ time perl MakeAlbum.pl handyman.aws; } 2>> timing.txt
+
+echo -e "\nTime to sync to S3" >> timing.txt
 { time ./synctos3.sh; } 2>> timing.txt
 
 current_date_time=$(date +"%m/%d/%Y %H:%M")
-echo "finished at $current_date_time" >> timing.txt
+echo -e "\nfinished at $current_date_time" >> timing.txt
 
 cat timing.txt
--- synctos3.sh
+++ synctos3.sh
@@ -5,9 +5,17 @@ aws s3 cp ~/albums/index.html s3://album.donie.us/
 echo Syncing album 'projects'
 aws s3 sync ~/albums/projects/ s3://album.donie.us/projects --delete
 
+echo Syncing album 'ancestry'
+aws s3 sync ~/albums/ancestry/ s3://album.donie.us/ancestry --delete
+
 echo Syncing album 'eastdale'
 aws s3 sync ~/albums/eastdale/ s3://album.donie.us/eastdale --delete
+
+echo Syncing album 'eastthird'
+aws s3 sync ~/albums/eastthird/ s3://album.donie.us/eastthird --delete
 
 echo Syncing album 'incoming'
 aws s3 sync ~/albums/incoming/ s3://album.donie.us/incoming --delete
 
+echo Syncing content from ~/Google Drive/Handyman/website/httrack/webnode-site/i-can-do-that-handyman.webnode.page to S3
+aws s3 sync ~/Google\ Drive/Handyman/website/httrack/webnode-site/i-can-do-that-handyman.webnode.page/ s3://www.icandothathandyman.com/ --delete
