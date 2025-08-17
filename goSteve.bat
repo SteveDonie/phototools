2eb5dd74313d9d290dbe6a0d85f837dedea5e078
Steve Donie <steve@donie.us>
2025-08-16 23:52:47 -0500

    cleanup, training if wanted

--- goSteve.bat
+++ goSteve.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Making just Steve album and then syncing to S3
 
-REM Get current date and time
+:: Get current date and time
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
@@ -10,13 +10,19 @@ set "current_date_time=%MM%/%DD%/%YYYY% %HH%:%Min%"
 echo. > timing.txt
 echo Started at %current_date_time% >> timing.txt
 
-REM Copy index file (adjust paths as needed for Windows)
+:: Copy index file (adjust paths as needed for Windows)
+echo Copying index file
 copy "%USERPROFILE%\personal\donie.us\album-index.html" "%USERPROFILE%\albums\index.html"
+
+:: if this is on, it will always refresh the faces database and always process all the
+:: photos. Comment this out when in production mode.
+::echo goSteve.bat running face recognition training
+::perl TrainFaces.pl personal.aws train
 
 echo. >> timing.txt
 echo Time to make Personal >> timing.txt
 
-REM Time the Perl command (Windows doesn't have built-in time command like bash)
+:: Time the Perl command (Windows doesn't have built-in time command like bash)
 echo %TIME% - Starting MakeAlbum.pl >> timing.txt
 perl MakeAlbum.pl Personal.aws
 echo %TIME% - Finished MakeAlbum.pl >> timing.txt
@@ -24,12 +30,12 @@ echo %TIME% - Finished MakeAlbum.pl >> timing.txt
 echo. >> timing.txt
 echo Time to sync to S3 >> timing.txt
 
-REM Time the sync script
+:: Time the sync script
 echo %TIME% - Starting syncSteve.bat >> timing.txt
 call syncSteve.bat
 echo %TIME% - Finished syncSteve.bat >> timing.txt
 
-REM Get current date and time again
+:: Get current date and time again
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
@@ -38,5 +44,5 @@ set "current_date_time=%MM%/%DD%/%YYYY% %HH%:%Min%"
 echo. >> timing.txt
 echo finished at %current_date_time% >> timing.txt
 
-REM Display the timing file contents
+:: Display the timing file contents
 type timing.txt
